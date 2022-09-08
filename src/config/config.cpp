7ea6de51da65fbaeb558c9bf8537b54e5a22bbfa
fraxer <fraxer@mail.ru>
2022-09-09 00:58:06 +0700

    feat: implement config module functions

--- src/config/config.cpp
+++ src/config/config.cpp
@@ -1,16 +1,18 @@
 #include "config.h"
 
-char* getPathConfigFile(int argc, char* argv[]) {
+static char* CONFIG_PATH = nullptr;
 
+char* getConfigPath(int argc, char* argv[]) {
     int opt = 0;
-    opterr = 0;
-    optopt = 0;
-    optind = 0;
-    optarg = nullptr;
 
     char* path;
 
     bool c_found = false;
+
+    opterr = 0;
+    optopt = 0;
+    optind = 0;
+    optarg = nullptr;
 
     while ((opt = getopt(argc, argv, "c:")) != -1) {
         switch (opt) {
@@ -34,15 +36,41 @@ char* getPathConfigFile(int argc, char* argv[]) {
     return path;
 }
 
-void loadConfig(const char* path) {
+void saveConfigPath(const char* path) {
+    if (path == nullptr) {
+        throw "Config path is nullptr";
+    }
 
+    if (strlen(path) == 0) {
+        throw "Config path is empty";
+    }
+
+    if (CONFIG_PATH != nullptr) {
+        free(CONFIG_PATH);
+    }
+
+    CONFIG_PATH = (char*)malloc(sizeof(char) * strlen(path) + 1);
+
+    if (CONFIG_PATH == nullptr) {
+        throw "Error allocate memory for config path";
+    }
+
+    strcpy(CONFIG_PATH, path);
+}
+
+void loadConfig(const char* path) {
     int fd = open(path, O_RDONLY);
 
     if (fd == -1) throw "Config file not found";
 
-    // char* data = readFile(fd);
+    char* data = readFile(fd);
 
-    // ...
+    // proccessing
+    throw "proccessing";
 
     close(fd);
+}
+
+void reloadConfig() {
+    loadConfig(CONFIG_PATH);
 }
--- src/config/config.h
+++ src/config/config.h
@@ -1,6 +1,17 @@
+#ifndef __CONFIG__
+#define __CONFIG__
+
 #include <fcntl.h>
 #include <unistd.h>
+#include <cstring>
+#include "../helpers/helpers.h"
 
-char* getPathConfigFile(int argc, char* argv[]);
+char* getConfigPath(int argc, char* argv[]);
+
+void saveConfigPath(const char* path);
 
 void loadConfig(const char* path);
+
+void reloadConfig();
+
+#endif
