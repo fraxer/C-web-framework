e4fe4b801638b33585b4f79059a61e73dd338d45
fraxer <fraxer@mail.ru>
2024-05-19 02:31:40 +0700

    feat: add parser of the variable tag

--- libs/view/viewparser.c
+++ libs/view/viewparser.c
@@ -1,5 +1,14 @@
 #include "storage.h"
+#include "log.h"
 #include "viewparser.h"
+
+static int __viewparser_readfile(viewparser_t* parser, const char* path);
+static int __viewparser_parse_content(viewparser_t* parser);
+static int __viewparser_variable_create(viewparser_t* parser);
+static void __viewparser_variable_free(viewparser_variable_t* variable);
+static void __viewparser_variable_last_remove(viewparser_t* parser);
+static int __viewparser_variable_item_create(viewparser_variable_t* variable);
+static void __viewparser_variable_item_free(viewparser_variable_item_t* item);
 
 viewparser_t* viewparser_init(const jsondoc_t* document, const char* storage_name, const char* path) {
     viewparser_t* parser = malloc(sizeof * parser);
@@ -8,35 +17,282 @@ viewparser_t* viewparser_init(const jsondoc_t* document, const char* storage_nam
     parser->document = document;
     parser->storage_name = storage_name;
     parser->path = path;
-    parser->content = NULL;
+
+    parser->buffer = NULL;
+    parser->stage = VIEWPARSER_TEXT;
+    parser->symbol = VIEWPARSER_DEFAULT;
+    parser->bytes_readed = 0;
+    parser->pos_start = 0;
+    parser->pos = 0;
+    parser->variable = NULL;
+    parser->last_variable = NULL;
+
+    bufferdata_init(&parser->variable_buffer);
+    bufferdata_init(&parser->buf);
 
     return parser;
 }
 
 int viewparser_run(viewparser_t* parser) {
-    file_t file = storage_file_get(parser->storage_name, parser->path);
-    if (!file.ok)
+    if (!__viewparser_readfile(parser, parser->path)) {
+        log_error("root file not found %s", parser->path);
         return 0;
+    }
 
-    char* file_content = file.content(&file);
-    // if (file_content != NULL) {
-        
-    // }
-
-    file.close(&file);
-    
-    parser->content = file_content;
+    __viewparser_parse_content(parser);
 
     return 1;
 }
 
 char* viewparser_get_content(viewparser_t* parser) {
-    return parser->content;
+    bufferdata_complete(&parser->buf);
+
+    return bufferdata_copy(&parser->buf);
 }
 
 void viewparser_free(viewparser_t* parser) {
     if (parser == NULL)
         return;
 
+    if (parser->buf.dynamic_buffer) free(parser->buf.dynamic_buffer);
+    parser->buf.dynamic_buffer = NULL;
+
+    bufferdata_reset(&parser->buf);
+
     free(parser);
+}
+
+int __viewparser_readfile(viewparser_t* parser, const char* path) {
+    file_t file = storage_file_get(parser->storage_name, path);
+    if (!file.ok)
+        return 0;
+
+    parser->buffer = file.content(&file);
+    parser->bytes_readed = file.size;
+
+    file.close(&file);
+
+    return 1;
+}
+
+int __viewparser_parse_content(viewparser_t* parser) {
+    for (parser->pos = parser->pos_start; parser->pos < parser->bytes_readed; parser->pos++) {
+        const char ch = parser->buffer[parser->pos];
+
+        switch (parser->stage)
+        {
+        case VIEWPARSER_TEXT:
+        {
+            switch (ch)
+            {
+            case '{':
+            {
+                if (parser->symbol == VIEWPARSER_FIGFIGOPEN) {
+                    parser->stage = VIEWPARSER_VARIABLE;
+                    if (!__viewparser_variable_create(parser))
+                        return 0;
+
+                    if (!__viewparser_variable_item_create(parser->last_variable))
+                        return 0;
+
+                    bufferdata_pop_back(&parser->buf);
+                    break;
+                }
+
+                parser->symbol = VIEWPARSER_FIGFIGOPEN;
+                bufferdata_push(&parser->buf, ch);
+
+                break;
+            }
+            default:
+                bufferdata_push(&parser->buf, ch);
+                break;
+            }
+
+            break;
+        }
+        case VIEWPARSER_VARIABLE:
+        {
+            switch (ch)
+            {
+            case '}':
+            {
+                if (parser->symbol == VIEWPARSER_FIGFIGCLOSE) {
+                    parser->stage = VIEWPARSER_TEXT;
+                    break;
+                }
+
+                if (parser->symbol == VIEWPARSER_VARIABLE_DOT)
+                    return 0;
+
+                parser->symbol = VIEWPARSER_FIGFIGCLOSE;
+
+                while (1) {
+                    if (bufferdata_back(&parser->variable_buffer) != ' ')
+                        break;
+
+                    bufferdata_pop_back(&parser->variable_buffer);
+                }
+
+                bufferdata_complete(&parser->variable_buffer);
+                const size_t variable_item_size = bufferdata_writed(&parser->variable_buffer);
+                if (variable_item_size >= VIEWPARSER_VARIABLE_ITEM_NAME_SIZE)
+                    return 0;
+
+                if (variable_item_size > 0) {
+                    const int index = 0;
+                    parser->last_variable->last_item->index = index;
+                    strcpy(parser->last_variable->last_item->name, bufferdata_get(&parser->variable_buffer));
+                    parser->last_variable->last_item->name_length = variable_item_size;
+                }
+
+                const jsontok_t* object = json_root(parser->document);
+                if (!json_is_object(object))
+                    return 0;
+
+                const jsontok_t* token = json_object_get(object, parser->last_variable->last_item->name);
+                if (json_is_string(token)) {
+                    const char* variable_value = json_string(token);
+                    for (size_t i = 0; i < strlen(variable_value); i++) {
+                        bufferdata_push(&parser->buf, variable_value[i]);
+                    }
+                }
+
+                bufferdata_reset(&parser->variable_buffer);
+                __viewparser_variable_last_remove(parser);
+
+                break;
+            }
+            case ' ':
+            {
+                if (parser->symbol == VIEWPARSER_FIGFIGOPEN)
+                    break;
+
+                bufferdata_push(&parser->variable_buffer, ch);
+
+                break;
+            }
+            case '[':
+            {
+                if (parser->symbol == VIEWPARSER_FIGFIGOPEN)
+                    return 0;
+
+                break;
+            }
+            case ']':
+            {
+                if (parser->symbol == VIEWPARSER_FIGFIGOPEN)
+                    return 0;
+
+                break;
+            }
+            case '.':
+            {
+                if (parser->symbol == VIEWPARSER_FIGFIGOPEN)
+                    return 0;
+
+                parser->symbol = VIEWPARSER_VARIABLE_DOT;
+                break;
+            }
+            default:
+                parser->symbol = VIEWPARSER_DEFAULT;
+                bufferdata_push(&parser->variable_buffer, ch);
+                break;
+            }
+
+            break;
+        }
+        default:
+            break;
+        }
+    }
+
+    return 1;
+}
+
+int __viewparser_variable_create(viewparser_t* parser) {
+    viewparser_variable_t* variable = malloc(sizeof * variable);
+    if (variable == NULL) return 0;
+
+    variable->item = NULL;
+    variable->last_item = NULL;
+    variable->next = NULL;
+
+    if (parser->variable == NULL)
+        parser->variable = variable;
+
+    if (parser->last_variable != NULL)
+        parser->last_variable->next = variable;
+
+    parser->last_variable = variable;
+
+    return 1;
+}
+
+void __viewparser_variable_free(viewparser_variable_t* variable) {
+    if (variable == NULL) return;
+
+    viewparser_variable_item_t* item = variable->item;
+    while (item) {
+        viewparser_variable_item_t* next = item->next;
+        __viewparser_variable_item_free(item);
+
+        item = next;
+    }
+
+    variable->item = NULL;
+    variable->last_item = NULL;
+    variable->next = NULL;
+
+    free(variable);
+}
+
+void __viewparser_variable_last_remove(viewparser_t* parser) {
+    viewparser_variable_t* variable = parser->variable;
+    viewparser_variable_t* last_variable = parser->last_variable;
+    if (variable == last_variable) {
+        parser->variable = NULL;
+        parser->last_variable = NULL;
+    }
+    else {
+        while (variable) {
+            if (variable->next == parser->last_variable) {
+                variable->next = NULL;
+                parser->last_variable = variable;
+                break;
+            }
+
+            variable = variable->next;
+        }
+    }
+
+    __viewparser_variable_free(last_variable);
+}
+
+int __viewparser_variable_item_create(viewparser_variable_t* variable) {
+    if (variable == NULL) return 0;
+
+    viewparser_variable_item_t* item = malloc(sizeof * item);
+    if (item == NULL) return 0;
+
+    item->index = 0;
+    item->next = NULL;
+    item->name[0] = 0;
+    item->name_length = 0;
+
+    if (variable->item == NULL)
+        variable->item = item;
+
+    if (variable->last_item != NULL)
+        variable->last_item->next = item;
+
+    variable->last_item = item;
+
+    return 1;
+}
+
+void __viewparser_variable_item_free(viewparser_variable_item_t* item) {
+    if (item == NULL) return;
+
+    free(item);
 }
--- libs/view/viewparser.h
+++ libs/view/viewparser.h
@@ -2,12 +2,60 @@
 #define __VIEWPARSER__
 
 #include "json.h"
+#include "bufferdata.h"
+
+// typedef struct viewparser_content {
+//     char* content;
+//     struct viewparser_content* next;
+// } viewparser_content_t;
+#define VIEWPARSER_VARIABLE_ITEM_NAME_SIZE 80
+
+typedef struct viewparser_variable_item {
+    int index;
+    char name[VIEWPARSER_VARIABLE_ITEM_NAME_SIZE];
+    size_t name_length;
+    struct viewparser_variable_item* next;
+} viewparser_variable_item_t;
+
+typedef struct viewparser_variable {
+    viewparser_variable_item_t* item;
+    viewparser_variable_item_t* last_item;
+    struct viewparser_variable* next;
+} viewparser_variable_t;
+
+typedef enum viewparser_stage {
+    VIEWPARSER_TEXT = 0,
+    VIEWPARSER_VARIABLE,
+    VIEWPARSER_BRANCH,
+    VIEWPARSER_INCLUDE,
+} viewparser_stage_e;
+
+typedef enum viewparser_symbol {
+    VIEWPARSER_DEFAULT = 0,
+    VIEWPARSER_FIGFIGOPEN,
+    VIEWPARSER_FIGFIGCLOSE,
+    VIEWPARSER_FIGPERCENTOPEN,
+    VIEWPARSER_FIGPERCENTCLOSE,
+    VIEWPARSER_FIGASTOPEN,
+    VIEWPARSER_FIGASTCLOSE,
+    VIEWPARSER_VARIABLE_DOT,
+} viewparser_symbol_e;
 
 typedef struct {
     const jsondoc_t* document;
     const char* storage_name;
     const char* path;
-    char* content;
+    bufferdata_t variable_buffer;
+
+    char* buffer;
+    viewparser_stage_e stage;
+    viewparser_symbol_e symbol;
+    bufferdata_t buf;
+    size_t bytes_readed;
+    size_t pos_start;
+    size_t pos;
+    viewparser_variable_t* variable;
+    viewparser_variable_t* last_variable;
 } viewparser_t;
 
 viewparser_t* viewparser_init(const jsondoc_t* document, const char* storage_name, const char* path);
