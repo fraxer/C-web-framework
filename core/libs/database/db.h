f67f4ee8d2fff44b90cbe47aee302e42549fb04e
fraxer <fraxer@mail.ru>
2024-12-22 18:37:49 +0700

    chore: remove core and cmake directories

--- core/libs/database/db.h
+++
@@ -1,7 +0,0 @@
-#ifndef __DB__
-#define __DB__
-
-#include "dbquery.h"
-#include "dbresult.h"
-
-#endif
--- core/libs/response/response.h
+++
@@ -1,9 +0,0 @@
-#ifndef __RESPONSE__
-#define __RESPONSE__
-
-typedef struct response {
-    void(*reset)(void*);
-    void(*free)(void*);
-} response_t;
-
-#endif
